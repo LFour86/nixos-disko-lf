dfee702da500e3be12ff2a058da5b9c060d78817
LFour <63118374+LFour86@users.noreply.github.com>
2026-04-22 17:10:28 +0800

    Modify user password settings and file system paths
    Updated user password configurations and changed Flatpak file system path.

--- configuration.nix
+++ configuration.nix
@@ -35,13 +35,13 @@
     mutableUsers = false;
     users = {
       root = {
-        initialPassword = "your_password";
-        #hashedPasswordFile = "/persist/passwords/root";
+        #initialPassword = "your_password";
+        hashedPasswordFile = "/persist/passwords/root";
       };
       lfour = {
         isNormalUser = true;
-        initialPassword = "your_password";
-        #hashedPasswordFile = "/persist/passwords/lfour";
+        #initialPassword = "your_password";
+        hashedPasswordFile = "/persist/passwords/lfour";
         description = "LFour";
         extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
       };
@@ -96,7 +96,7 @@
     options = [ "compress=zstd:3" "noatime" "discard=async" "space_cache=v2" "ssd" "commit=120" ];
     neededForBoot = true;
   };
-  fileSystems."/flatpak" = { options = [ "compress=zstd:3" "noatime" "discard=async" "space_cache=v2" "ssd" "commit=120" ]; };
+  fileSystems."/var/lib/flatpak" = { options = [ "compress=zstd:3" "noatime" "discard=async" "space_cache=v2" "ssd" "commit=120" ]; };
   fileSystems."/nix" = { options = [ "compress=zstd:3" "noatime" "discard=async" "space_cache=v2" "ssd" "commit=120" ]; };
 
   # swap
@@ -111,6 +111,7 @@
       #"/var/lib"  # suggested subdirectory breakdown
       "/var/lib/bluetooth"
       "/var/lib/systemd/coredump"
+      "/var/lib/systemd/random-seed"
       "/var/lib/libvirt"
       "/var/lib/docker"
 
