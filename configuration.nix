c3d5a7f191d720542f0b860690541d88f76d4487
LFour <63118374+LFour86@users.noreply.github.com>
2026-04-22 16:37:00 +0800

    Update file system configuration with new directories
    Added additional directories for system services under /var/lib.

--- configuration.nix
+++ configuration.nix
@@ -95,7 +95,8 @@
   fileSystems."/persist" = {
     options = [ "compress=zstd:3" "noatime" "discard=async" "space_cache=v2" "ssd" "commit=120" ];
     neededForBoot = true;
-    };
+  };
+  fileSystems."/flatpak" = { options = [ "compress=zstd:3" "noatime" "discard=async" "space_cache=v2" "ssd" "commit=120" ]; };
   fileSystems."/nix" = { options = [ "compress=zstd:3" "noatime" "discard=async" "space_cache=v2" "ssd" "commit=120" ]; };
 
   # swap
@@ -107,11 +108,20 @@
     directories = [
       "/etc/nixos"
       "/var/log"
-      "/var/lib"  # suggested subdirectory breakdown
+      #"/var/lib"  # suggested subdirectory breakdown
+      "/var/lib/bluetooth"
+      "/var/lib/systemd/coredump"
+      "/var/lib/libvirt"
+      "/var/lib/docker"
+
       "/etc/NetworkManager/system-connections"
+
+      # If use SSH
+      "/etc/ssh"
     ];
     files = [
       "/etc/machine-id"
+
       # If use SSH
       # "/etc/ssh/ssh_host_rsa_key"
       # "/etc/ssh/ssh_host_ed25519_key"
