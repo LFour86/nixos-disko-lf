8dbf03039e554f98685a690b724f68f2cd28f59d
LFour <63118374+LFour86@users.noreply.github.com>
2026-07-20 21:42:09 +0800

    feat: add new impermanence dir

--- configuration.nix
+++ configuration.nix
@@ -113,6 +113,7 @@
       "/var/lib/AccountsService"
       "/var/lib/bluetooth"
       "/var/lib/cups"
+      "/var/lib/hermes"
       "/var/lib/NetworkManager"
       "/var/lib/nixos"
       "/var/lib/systemd/backlight"
